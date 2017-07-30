df8ca4f2350980ac892b820544694e94699fa630
Dai <dailectic@gmail.com>
2017-07-29 18:40:40 -0700

    added Data default to EqC

--- src/Data/Constructors/EqC.hs
+++ src/Data/Constructors/EqC.hs
@@ -1,6 +1,11 @@
+{-# language DefaultSignatures #-}
 module Data.Constructors.EqC (EqC(..)) where
+import Data.Function (on)
+import Data.Data (Data,toConstr)
 
 class EqC a where
   -- | Compare the outermost constructor for a datatype.
   -- Instances should satisfy @eqConstr (C a) (K b) = True@ iff C=K
   eqConstr :: a -> a -> Bool
+  default eqConstr :: Data a => a -> a -> Bool
+  eqConstr = on (==) toConstr
